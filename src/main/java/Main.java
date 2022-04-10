0198b33c13710c25981d2b18fa55db9bb2002ef2
Dadavai <Dadavai@DESKTOP-5VMKOGH.fios-router.home>
2022-04-10 14:00:06 -0400

    added clarification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
     int x = 5;
 
   public static void main(String[] args) {
-    Categories Pro = new Professional(new Tops());
+    Categories Pro = new Professional(new Tops("12","red","silk"));
     
     
   }

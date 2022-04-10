42a40f66a458f0e8aa03c158e1f02bd75c639dcf
Dadavai <Dadavai@DESKTOP-5VMKOGH.fios-router.home>
2022-04-10 12:44:12 -0400

    a main

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Dadavai
+ */
+public class Main {
+    int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+    
+}
